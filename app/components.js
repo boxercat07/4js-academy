109040ad52aabfca918f6feb27d15050cdc2d05e
Vincent Martinet <vincent.martinet@volarisgroup.com>
2026-03-25 16:21:44 +0100

    feat(ux): restrict header rating interaction until track completion

--- app/components.js
+++ app/components.js
@@ -4909,7 +4909,7 @@ class AiRating extends HTMLElement {
         const outlineStar = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="${starSize}"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.536a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /></svg>`;
 
         this.innerHTML = `
-            <div class="flex items-center ${containerClass}">
+            <div class="flex items-center ${containerClass} select-none cursor-inherit">
                 <div class="flex text-amber-500 shrink-0">
                     ${[1, 2, 3, 4, 5]
                         .map(
